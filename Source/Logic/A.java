1f42715e973d33ef4fea6e058a8eebd15531e06b
jniewinski <jakubniewinski@yahoo.com>
2019-12-02 16:14:12 +0100

    komentarze

--- Source/Logic/A.java
+++ Source/Logic/A.java
@@ -21,34 +21,34 @@ public class A {
 
     public static Vector<Coords> minimum(ArrayList<Vector<Coords>> Way, Coords end)
     {
-        double minCost=1000000;
+        double minCost=1000000; //inicjalizacja min kosztu
         Vector<Coords> temp =new Vector<Coords>();
         Vector<Coords> vector;
 
         for(int it=0; it<Way.size(); it++ )  //szukanie minimum
         {
             vector=Way.get(it);
-            if (vector.size() + heuristics(end, vector.lastElement()) < minCost)
+            if (vector.size() + heuristics(end, vector.lastElement()) < minCost)  //jesli mniejsze niz koszt+heurystyka
             {
                 minCost=vector.size() + heuristics(end, vector.lastElement());
                 temp=vector;
             }
 
         }
-      //  System.out.println("Minimum: "+minCost);
-        return temp;
+
+        return temp; // ret minimum
     }
 
 
 
     public static Vector<Coords> A()
-    {  // int maxIterations=100000;
-        ArrayList<Vector<Coords>> Way=new ArrayList<Vector<Coords>>();
-        Coords start, end;
+    {
+        ArrayList<Vector<Coords>> Way=new ArrayList<Vector<Coords>>(); // lista przechowujaca vektory ktore sa scizkami
+        Coords start, end; // Coords przechowuje tylko x,y - koordynaty
         start = new Coords();
         end = new Coords();
 
-        for (int i=0; i< InitMap.x; i++)  // wspolrzedne poczatku i konca
+        for (int i=0; i< InitMap.x; i++)  // szukanie  wspolrzednych poczatku i konca
         {
             for (int j=0; j<InitMap.y; j++)
             {
@@ -66,20 +66,19 @@ public class A {
             }
         }
 
-        Vector<Coords> vector =new Vector<Coords>();
+        Vector<Coords> vector =new Vector<Coords>(); //pierwsza sciezka
         Vector<Coords> temp;
 
 
-        vector.add(start);
-        Way.add(vector);
+        vector.add(start); //dodanie do niej startu
+        Way.add(vector);  // i wpisanie vektora do listy sciezek
 
-        int minCost=1000000;
         int k=0;
         while(k<InitMap.maxIterations) {  //szukanie do bolu
             k++;
-            temp = minimum(Way,end);
-            Way.remove(temp);
-            Coords current=temp.lastElement();
+            temp = minimum(Way,end); //szukanie minimum w kazdej iteracji
+            Way.remove(temp);          // usuniecie minimalnej sciezki bo chcemy tylko jej rozwiniecia
+            Coords current=temp.lastElement(); //pobranie ostatniego elementu tej sciezki
 
            for (int i =-1; i<2 ; i++)
            {
@@ -91,30 +90,35 @@ public class A {
 
                        if(InitMap.Mapa[current.x+i][current.y+j]==3) // jesli znalezlismy koniec
                        {
-                        vector=temp;
-                        vector.remove(start);
+                        vector=temp;  //wpisanie tymczasowego wektoru do naszego zwracanego
+                        vector.remove(start); // nie chcemy przykryc punktu startowego przy wyswietlaniu
                            A.result = true;
-                           System.out.println(k);
-                        return vector;
+                        return vector; // ret
                        }
                        else
                        {
-                           if(InitMap.Mapa[current.x+i][current.y+j]==0)
+                           if(InitMap.Mapa[current.x+i][current.y+j]==0)  // jesli puste pole to rozwijamy w ta strone sciezke
                            {
-                            AtomicInteger flag= new AtomicInteger(0);
-                            Coords next=new Coords(current.x+i,current.y+j);
+                            AtomicInteger flag= new AtomicInteger(0); // w wyrazeniach lambda zmienne musza byc atomowe
+                               // w naszym rozwiazaniu dziala identycznie jak zwykly int
+
+                            Coords next=new Coords(current.x+i,current.y+j); // nowe pole do wpisania do wektoru
+                                                                             // poprzednie koordynaty zwiekszone o +- 1
+                               // oba wyrazenia ponizej sluza do tego zeby traktowac mape jako graf
 
                                 temp.forEach(iterator -> {
-                                    if(iterator.x==next.x && iterator.y==next.y) {
-                                        flag.set(1);
+                                    if(iterator.x==next.x && iterator.y==next.y) {  // nie chcemy stac w miejscu
+                                                                                    // raczej sie nie zdarzy ale w razie czego
+                                        flag.set(1); // flaga czy mozna rozwinac
 
                                     }
                                     Way.forEach(list ->{
-                                        if(list.lastElement().x==next.x && list.lastElement().y==next.y)
+                                        if(list.lastElement().x==next.x && list.lastElement().y==next.y) //nie chcemy sie cofac
                                             flag.set(1);
                                     });
 
                                 });
+
                                 if (flag.get()==1)
                                     {
                                         flag.set(0);
@@ -124,8 +128,8 @@ public class A {
                                 else
                                     {
                                         Vector<Coords> newVector = (Vector) temp.clone();
-                                        newVector.add(next);
-                                        Way.add(newVector);
+                                        newVector.add(next);    // jesli wszystko okej to wpisanie do vektora nowgo pola
+                                        Way.add(newVector);     // i dodanie nowego vektora do listy
                                     }
 
                            }
@@ -138,10 +142,10 @@ public class A {
 
         }
         if (k==InitMap.maxIterations){
-            System.out.println("Nie mozna znalezc sciezki w podanej liczbie iteracji");
-            vector.removeAllElements();
+	    System.out.println("Nie mozna znalezc sciezki w podanej liczbie iteracji"); // jesli nie zdazylo znalezc to o tym powiadom
+            vector.removeAllElements();                      // wyczysczenie tego vektora
 
         }
-        return vector;
+        return vector; // zwrocenie pustego vektora (skoro nie bylo rozwiazania nie bedziemy go rysowac)
     }
 }
